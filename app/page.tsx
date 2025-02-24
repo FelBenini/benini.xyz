21de46fe0589299ecd0eaf3a2929c4b6bb9c19e7
FelBenini <felbenini@gmail.com>
2025-02-23 21:37:56 -0300

    Added buttons on hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { MorphingText } from "@/components/ui/morphing-text";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { BlurFade } from "@/components/ui/blur-fade";
+import { RainbowButton } from "@/components/ui/rainbow-button";
+import { ShimmerButton } from "@/components/ui/shimmer-button";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -12,6 +15,18 @@ export default function Page() {
         <h1 className="font-bold text-[38pt] md:text-[56px] lg:text-[6rem] text-center popin">Felipe Benini</h1>
       </BlurFade>
       <MorphingText texts={["fullstack", "backend", "frontend", "devops"]} className="text-[64px] popin text-center mx-auto" />
+      <BlurFade delay={0.5} className="mt-20 flex flex-wrap justify-center items-center w-full">
+        <Link href="/contact" className="mx-[calc(11px+2.5%)] mb-10">
+          <RainbowButton className="text-nowrap">
+            Get in Contact
+          </RainbowButton>
+        </Link>
+        <Link href="https://github.com/FelBenini" target="_blank" className="mx-[calc(12px+2.5%)] mb-10">
+          <ShimmerButton className="text-[white] px-[3.4rem]">
+            <p className="text-[white]">Check out my GitHub!</p>
+          </ShimmerButton>
+        </Link>
+      </BlurFade>
     </AuroraBackground>
   );
 }
